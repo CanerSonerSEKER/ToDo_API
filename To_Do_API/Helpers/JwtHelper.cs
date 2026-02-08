651a1734ab101a5b966607f5bb37bcc2a46e9446
canersonerseker <canerseker444@gmail.com>
2026-02-08 14:38:03 +0300

    feat: GenerateToken methodu jwthelper içerisinde oluşturuldu.

--- To_Do_API/Helpers/JwtHelper.cs
+++ To_Do_API/Helpers/JwtHelper.cs
@@ -1,6 +1,55 @@
-﻿namespace To_Do_API.Helpers
+﻿using Microsoft.IdentityModel.Tokens;
+using NuGet.Common;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using To_Do_API.Models;
+
+namespace To_Do_API.Helpers
 {
     public class JwtHelper
     {
+        private readonly IConfiguration _configuration;
+
+        public JwtHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            IConfiguration jwtSettings = _configuration.GetSection("Jwt");
+            byte[] key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+            Claim[] claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.StreetAddress, user.Address),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var credentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256
+                );
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["ToDoAPI"],
+                audience: jwtSettings["ToDoAPIUsers"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtSettings["ExpiresMinutes"])),
+                signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+
+
+
+
+
     }
 }
