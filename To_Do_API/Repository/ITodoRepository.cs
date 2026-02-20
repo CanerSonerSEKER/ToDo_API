8be9616b2573e39e9de59ab684bbc006a1ca4cfc
canersonerseker <canerseker444@gmail.com>
2026-02-20 18:29:25 +0300

    docs: repository dosyası oluşturuldu.

---
+++ To_Do_API/Repository/ITodoRepository.cs
@@ -0,0 +1,12 @@
+﻿using To_Do_API.Models.ToDoDTO;
+
+namespace To_Do_API.Repository
+{
+    public interface ITodoRepository
+    {
+        Task<IEnumerable<TodoItemDTO>> GetAllAsync(long id);
+
+
+
+    }
+}
