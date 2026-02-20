a5ba2484f09371ed4876b81c397b795c78887a0b
canersonerseker <canerseker444@gmail.com>
2026-02-20 18:35:48 +0300

    docs: ITodoRepository oluşturuldu.

--- To_Do_API/Repository/ITodoRepository.cs
+++ To_Do_API/Repository/ITodoRepository.cs
@@ -4,8 +4,11 @@ namespace To_Do_API.Repository
 {
     public interface ITodoRepository
     {
-        Task<IEnumerable<TodoItemDTO>> GetAllAsync(long id);
-
+        Task<IEnumerable<TodoItemDTO>> GetAllAsync(long userId);
+        Task<TodoItemDTO> GetByIdAsync(long id, long userId);
+        Task<TodoItemDTO> CreateAsync(CreateTodoItemRequest createRequest, long userId, CancellationToken ct);
+        Task<TodoItemDTO> UpdateAsync(long id, long userId, UpdateTodoItemRequest updateRequest, CancellationToken ct);
+        Task DeleteAsync(long id, long userId);
 
 
     }
